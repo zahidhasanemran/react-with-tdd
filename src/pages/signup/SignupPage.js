0991e4f7816cf2bef4deadd388d79ce9db2e995f
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-07-27 12:03:17 +0600

    initialization and signup page created with test code first

---
+++ src/pages/signup/SignupPage.js
@@ -0,0 +1,96 @@
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+
+const SignupPage = () => {
+  const [disable, setDisable] = useState(true)
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  //   useEffect(() => {
+  //     let timer;
+  //     if (disable) {
+  //       timer = setTimeout(() => {
+  //         console.log("disable false");
+  //         setDisable(false);
+  //       }, 1000);
+  //     }
+
+  //     return () => clearTimeout(timer);
+  //   }, [disable]);
+
+  const Submit = (e) => {
+    e.preventDefault()
+    const body = {
+      username,
+      email,
+      password,
+    }
+    axios.post("http://localhost:8080/api/1.0/users", body)
+  }
+
+  useEffect(() => {
+    if (password && confirmPassword && password === confirmPassword) {
+      setDisable(false)
+    } else {
+      setDisable(true)
+    }
+  }, [password, confirmPassword])
+
+  return (
+    <div>
+      <h2> SIGN UP </h2>
+      <form>
+        <div className="singleInput">
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            id="username"
+            placeholder="username"
+          />
+        </div>
+        <div className="singleInput">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            // pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+            id="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="singleInput">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="password"
+            min="4"
+            max="6"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="singleInput">
+          <label htmlFor="passwordRepeat">Password repeat</label>
+          <input
+            type="password"
+            id="passwordRepeat"
+            min="4"
+            max="6"
+            placeholder="password repeat"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div className="singleInput">
+          <button onClick={(e) => Submit(e)} type="submit" disabled={disable}>
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignupPage
---
+++ src/pages/signup/SignupPage.spec.js
@@ -0,0 +1,88 @@
+/* eslint-disable testing-library/no-render-in-setup */
+import SignupPage from "./SignupPage"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import axios from "axios"
+
+describe("Sign up page", () => {
+  describe("Page Layout ", () => {
+    beforeEach(() => {
+      render(<SignupPage />)
+    })
+    it("has a heading with text Sign Up", () => {
+      const header = screen.getByRole("heading", { name: "SIGN UP" })
+      expect(header).toBeInTheDocument()
+    })
+
+    it("has username input", () => {
+      const username = screen.getByPlaceholderText("username")
+      expect(username).toBeInTheDocument()
+    })
+    it("has email input", () => {
+      const email = screen.getByPlaceholderText("email")
+      expect(email).toBeInTheDocument()
+    })
+    it("has password repeat input", () => {
+      const passwordrepeat = screen.getByPlaceholderText("password repeat")
+      expect(passwordrepeat).toBeInTheDocument()
+    })
+    it("has button wiht type submit", () => {
+      const submitButton = screen.getByRole("button", {
+        name: "Sign up",
+      })
+      expect(submitButton).toBeInTheDocument()
+    })
+    it("should be disabled by default", () => {
+      const submitButton = screen.getByRole("button", {
+        name: "Sign up",
+      })
+      expect(submitButton).toBeDisabled()
+    })
+  })
+
+  describe("Interaction", () => {
+    beforeEach(() => {
+      render(<SignupPage />)
+    })
+
+    it("enable submit button after typing password and repeat password", () => {
+      const pass = screen.getByPlaceholderText("password")
+      const passRep = screen.getByPlaceholderText("password repeat")
+      userEvent.type(pass, "p4ssword")
+      userEvent.type(passRep, "p4ssword")
+      const submitBtn = screen.getByRole("button", { name: "Sign up" })
+      expect(submitBtn).not.toBeDisabled()
+    })
+
+    it("sign in functionality check ", () => {
+      const username = screen.getByLabelText("Username")
+      const email = screen.getByLabelText("Email")
+      const pass = screen.getByPlaceholderText("password")
+      const passRep = screen.getByPlaceholderText("password repeat")
+      userEvent.type(username, "user1")
+      userEvent.type(email, "user1@mail.com")
+      userEvent.type(pass, "p4ssword")
+      userEvent.type(passRep, "p4ssword")
+      const submitBtn = screen.getByRole("button", { name: "Sign up" })
+
+      // create moc function with axios
+      const mockFn = jest.fn()
+      axios.post = mockFn
+
+      // clicking on submit button
+      userEvent.click(submitBtn)
+
+      // getting first call of calls list
+      const firstCallOfMockFunction = mockFn.mock.calls[0]
+
+      // extracting body form first call
+      const body = firstCallOfMockFunction[1]
+      console.log(firstCallOfMockFunction[0])
+      expect(body).toEqual({
+        username: "user1",
+        email: "user1@mail.com",
+        password: "p4ssword",
+      })
+    })
+  })
+})
